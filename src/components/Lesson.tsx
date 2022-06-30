9b0cd2360877ebcaac29923aa362642e631453d2
MJWORKS <mjworks2020@outlook.com>
2022-06-30 14:07:06 -0300

    Added icon in Lesson component

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,10 +1,13 @@
+import { CheckCircle } from "phosphor-react";
+
 export function Lesson() {
   return (
     <a href="#">
       <span className="text-gray-300">Terça * 22 de junho * 19h00</span>
       <div className="rouded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium">
+          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+            <CheckCircle size={20} />
             Conteúdo liberado
           </span>
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
