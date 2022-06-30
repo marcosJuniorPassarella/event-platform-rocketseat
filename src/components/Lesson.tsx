7b3bff5415acb7d3d03230189001c22169318717
MJWORKS <mjworks2020@outlook.com>
2022-06-30 14:36:58 -0300

    Added api request

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
 
 interface LessonProps {
   title: string;
@@ -8,22 +8,29 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+  const isLessonAvaiable = false;
+
   return (
     <a href="#">
-      <span className="text-gray-300">Terça * 22 de junho * 19h00</span>
+      <span className="text-gray-300">{props.avaiableAt.toString()}</span>
       <div className="rouded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
-          <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-            <CheckCircle size={20} />
-            Conteúdo liberado
-          </span>
+          {isLessonAvaiable ? (
+            <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+              <CheckCircle size={20} />
+              Conteúdo liberado
+            </span>
+          ) : (
+            <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+              <Lock size={20} />
+              Em breve
+            </span>
+          )}
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
-            Ao vivo
+            {props.type === "live" ? "AO VIVO" : "AULA PRÁTICA"}
           </span>
         </header>
-        <strong className="text-gray-200 mt-5 block">
-          Abertura do evento IgniteLab
-        </strong>
+        <strong className="text-gray-200 mt-5 block">{props.title}</strong>
       </div>
     </a>
   );
